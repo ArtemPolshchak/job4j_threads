c0820469dba47fb32fd430cf6980ae65793680bd
Артем Польщак <artem.polschak@gmail.com>
2023-10-20 03:22:25 +0300

    4. Standby mode. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Start loading ... ");
+
+                        for (int i = 0; i < 100; i++) {
+                            System.out.println("\rLoading : " + i + "%");
+                            Thread.sleep(1000);
+                        }
+                        System.out.println("Loaded : 100%");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+}
